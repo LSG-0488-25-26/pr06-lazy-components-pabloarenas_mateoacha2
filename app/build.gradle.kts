0e95349adba8b071d47a245f0de7059ac18d3e62
Mateo <mateo.acha@gracia.lasalle.cat>
2026-01-14 20:53:36 +0100

    Añadir dependencias de navegación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,16 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
+    
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    
+    // LiveData
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
